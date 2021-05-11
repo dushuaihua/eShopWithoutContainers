47eb364450a11fad5ecd87a9dffd63642c65a027
ShuaiHua Du <shuaihua.du@outlook.com>
2021-05-11 09:00:17 +0800

    Add account related process logic.

---
+++ Services/Identity/Identity.API/Controllers/AccountController.cs
@@ -0,0 +1,277 @@
+﻿using Identity.API.Models;
+using Identity.API.Models.AccountViewModels;
+using Identity.API.Services;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Identity.API.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILoginService<ApplicationUser> _loginService;
+        private readonly IIdentityServerInteractionService _interactionService;
+        private readonly IClientStore _clientStore;
+        private readonly ILogger<AccountController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(
+            ILoginService<ApplicationUser> loginService,
+             IIdentityServerInteractionService interactionService,
+             IClientStore clientStore,
+             ILogger<AccountController> logger,
+             UserManager<ApplicationUser> userManager,
+             IConfiguration configuration)
+        {
+            _loginService = loginService;
+            _interactionService = interactionService;
+            _clientStore = clientStore;
+            _logger = logger;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            var context = await _interactionService.GetAuthorizationContextAsync(returnUrl);
+
+            if (context?.IdP is not null)
+            {
+                throw new NotImplementedException("External login is not implemented!");
+            }
+            var vm = await BuildLoginViewModelAsync(returnUrl, context);
+            ViewData["ReturnUrl"] = returnUrl;
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _loginService.FindByUserName(model.Email);
+
+                if (await _loginService.ValidateCredentials(user, model.Password))
+                {
+                    var tokenLifetime = _configuration.GetValue("TokenLifetimeMinutes", 120);
+                    var props = new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(tokenLifetime),
+                        AllowRefresh = true,
+                        RedirectUri = model.ReturnUrl
+                    };
+
+                    if (model.RememberMe)
+                    {
+                        var permanentTokenLifetime = _configuration.GetValue("PermanentTokenLifetimeDays", 365);
+
+                        props.ExpiresUtc = DateTimeOffset.UtcNow.AddDays(permanentTokenLifetime);
+                        props.IsPersistent = true;
+                    }
+
+                    await _loginService.SignInAsync(user, props);
+
+                    if (_interactionService.IsValidReturnUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    return Redirect("~/");
+                }
+
+                ModelState.AddModelError("", "Invalid username or password.");
+            }
+            var vm = await BuildLoginViewModelAsync(model);
+            ViewData["ReturnUrl"] = model.ReturnUrl;
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout(string logoutId)
+        {
+            if (User.Identity.IsAuthenticated == false)
+            {
+                return await Logout(new LogoutViewModel { LogoutId = logoutId });
+            }
+
+            var context = await _interactionService.GetLogoutContextAsync(logoutId);
+
+            if (context?.ShowSignoutPrompt == false)
+            {
+                return await Logout(new LogoutViewModel { LogoutId = logoutId });
+            }
+
+            var vm = new LogoutViewModel
+            {
+                LogoutId = logoutId
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Logout(LogoutViewModel model)
+        {
+            var idp = User?.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
+            if (idp is not null && idp != IdentityServerConstants.LocalIdentityProvider)
+            {
+                if (model.LogoutId is null)
+                {
+                    model.LogoutId = await _interactionService.CreateLogoutContextAsync();
+                }
+                var url = "/account/logout?logoutId=" + model.LogoutId;
+                try
+                {
+                    await HttpContext.SignOutAsync(idp, new AuthenticationProperties
+                    {
+                        RedirectUri = url
+                    });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "LOGOUT ERROR:{ExceptionMessage}", ex.Message);
+                }
+            }
+            await HttpContext.SignOutAsync();
+
+            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+
+            HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity());
+
+            var logout = await _interactionService.GetLogoutContextAsync(model.LogoutId);
+
+            return Redirect(logout?.PostLogoutRedirectUri);
+        }
+
+        public async Task<IActionResult> DeviceLogout(string redirectUrl)
+        {
+            await HttpContext.SignOutAsync();
+
+            HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity());
+
+            return Redirect(redirectUrl);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    CardHolderName = model.User.CardHolderName,
+                    CardNumber = model.User.CardNumber,
+                    CardType = model.User.CardType,
+                    City = model.User.City,
+                    Country = model.User.Country,
+                    Expiration = model.User.Expiration,
+                    LastName = model.User.LastName,
+                    Name = model.User.Name,
+                    Street = model.User.Street,
+                    State = model.User.State,
+                    ZipCode = model.User.ZipCode,
+                    PhoneNumber = model.User.PhoneNumber,
+                    SecurityNumber = model.User.SecurityNumber
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Errors.Any())
+                {
+                    AddErrors(result);
+                    return View(model);
+                }
+            }
+
+            if (returnUrl is not null)
+            {
+                if (HttpContext.User.Identity.IsAuthenticated)
+                {
+                    return Redirect(returnUrl);
+                }
+                else if (ModelState.IsValid)
+                {
+                    return RedirectToAction("login", "account", new { returnUrl = returnUrl });
+                }
+                else
+                {
+                    return View(model);
+                }
+            }
+
+            return RedirectToAction("index", "home");
+        }
+
+        [HttpGet]
+        public IActionResult Redirecting()
+        {
+            return View();
+        }
+
+        private async Task<LoginViewModel> BuildLoginViewModelAsync(string returnUrl, AuthorizationRequest context)
+        {
+            var allowLocal = true;
+            if (context?.Client?.ClientId is not null)
+            {
+                var client = await _clientStore.FindEnabledClientByIdAsync(context.Client.ClientId);
+                if (client is not null)
+                {
+                    allowLocal = client.EnableLocalLogin;
+                }
+            }
+
+            return new LoginViewModel
+            {
+                ReturnUrl = returnUrl,
+                Email = context?.LoginHint
+            };
+        }
+
+        private async Task<LoginViewModel> BuildLoginViewModelAsync(LoginViewModel model)
+        {
+            var context = await _interactionService.GetAuthorizationContextAsync(model.ReturnUrl);
+            var vm = await BuildLoginViewModelAsync(model.ReturnUrl, context);
+            vm.Email = model.Email;
+            vm.RememberMe = model.RememberMe;
+            return vm;
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+    }
+}
---
+++ Services/Identity/Identity.API/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Identity.API.Models;
+using Identity.API.Services;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace Identity.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IIdentityServerInteractionService _interactionService;
+        private readonly IOptionsSnapshot<AppSettings> _settings;
+        private readonly IRedirectService _redirectService;
+
+        public HomeController(IIdentityServerInteractionService interactionService,
+            IOptionsSnapshot<AppSettings> settings, IRedirectService redirectService)
+        {
+            _interactionService = interactionService;
+            _settings = settings;
+            _redirectService = redirectService;
+        }
+
+        public IActionResult Index(string returnUrl)
+        {
+            return View();
+        }
+
+        public IActionResult ReturnToOriginalApplication(string returnUrl)
+        {
+            if (returnUrl is not null)
+            {
+                return Redirect(_redirectService.ExtractRedirectUriFromReturnUrl(returnUrl));
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public async Task<IActionResult> Error(string errorId)
+        {
+            var vm = new ErrorViewModel();
+
+            var message = await _interactionService.GetErrorContextAsync(errorId);
+
+            if (message is not null)
+            {
+                vm.Error = message;
+            }
+            return View("Error", vm);
+        }
+    }
+}
