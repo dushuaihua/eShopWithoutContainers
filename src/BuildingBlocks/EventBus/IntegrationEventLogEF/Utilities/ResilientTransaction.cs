a776e7a12538160f07a8cdb3ca82c174d49be6e4
ShuaiHua Du <shuaihua.du@outlook.com>
2021-10-23 20:16:36 +0800

    Add Integration Event Log EF.

---
+++ src/BuildingBlocks/EventBus/IntegrationEventLogEF/Utilities/ResilientTransaction.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace IntegrationEventLogEF.Utilities
+{
+    public class ResilientTransaction
+    {
+        private DbContext _context;
+
+        public ResilientTransaction(DbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public static ResilientTransaction New(DbContext context)
+        {
+            return new ResilientTransaction(context);
+        }
+
+        public async Task ExecuteAsync(Func<Task> action)
+        {
+            var strategy = _context.Database.CreateExecutionStrategy();
+            await strategy.ExecuteAsync(async () =>
+            {
+                using (var transaction = _context.Database.BeginTransaction())
+                {
+                    await action();
+                    transaction.Commit();
+                }
+            });
+        }
+    }
+}
