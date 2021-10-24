95e261b95e568eea9dba840f0731fe962c22891f
ShuaiHua Du <shuaihua.du@outlook.com>
2021-10-24 21:37:51 +0800

    Add Devspaces support.

---
+++ src/BuildingBlocks/Devspaces/Devspaces.Support/DevspacesMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Devspaces.Support
+{
+    public class DevspacesMessageHandler : DelegatingHandler
+    {
+        private const string DevspacesHeaderName = "azds-route-as";
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public DevspacesMessageHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var httpRequest = _httpContextAccessor.HttpContext.Request;
+            if (httpRequest.Headers.ContainsKey(DevspacesHeaderName))
+            {
+                request.Headers.Add(DevspacesHeaderName, httpRequest.Headers[DevspacesHeaderName] as IEnumerable<string>);
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
